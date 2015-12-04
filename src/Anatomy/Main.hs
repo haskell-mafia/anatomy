44d5e923496b48c89fa13716f3c86547ace2deb3
Nick Hibberd <nhibberd@gmail.com>
2015-12-04 22:36:31 +1100

    Inline environment variables into each call

--- src/Anatomy/Main.hs
+++ src/Anatomy/Main.hs
@@ -24,7 +24,6 @@ import           System.Posix.Env (getEnv)
 
 import           X.Options.Applicative hiding (VersionCommand)
 
-
 data Command =
     VersionCommand
   | ReportCommand
@@ -35,83 +34,87 @@ data Command =
   deriving (Eq, Show)
 
 anatomyMain ::
-     String
+     String -- ^ Build version
   -> (a -> Maybe GithubTemplate) -- ^ a function that takes the class of a project and decides which template to apply, if any at all...
-  -> Org        -- ^ GitHub organisation
-  -> Team       -- ^ A team of people to add as Collaborators on the new repo.
-  -> Team       -- ^ A team of everyone in your organisation
-  -> [Project a b]
+  -> Org -- ^ GitHub organisation
+  -> Team -- ^ A team of people to add as Collaborators on the new repo.
+  -> Team -- ^ A team of everyone in your organisation
+  -> [Project a b] -- ^ List of projects
   -> IO ()
 anatomyMain buildInfoVersion templates org owners everyone projects = do
   hSetBuffering stdout LineBuffering
   hSetBuffering stderr LineBuffering
 
-  github' <- env "GITHUB_OAUTH"
-  let github = GithubOAuth . T.unpack $ github'
-  jenkins <- JenkinsAuth <$> (getEnv "JENKINS_AUTH" >>=  pure . maybe github' T.pack)
-  user <- JenkinsUser <$> env "JENKINS_USER"
-  host <- JenkinsUrl <$> env "JENKINS_HOST"
-  hookz <- HooksUrl <$> env "JENKINS_HOOKS"
-
-  let jconf = JenkinsConfiguration user jenkins host
-
-  token <- HipchatToken <$> env "HIPCHAT_TOKEN"
-  room <- (HipchatRoom . T.pack) <$> (fromMaybe "eng" <$> getEnv "HIPCHAT_ROOM_GITHUB")
-
-
---        (orDie renderSyncError $ sync user  j h templateName o owners everyone projects s) >>= syncReport
   dispatch anatomy >>= \sc ->
     case sc of
       VersionCommand ->
         (putStrLn $ "anatomy: " <> buildInfoVersion) >> exitSuccess
 
-      -- not done
-      ReportCommand ->
-        orDie renderReportError $
-          report github org projects >>= mapM_ (\r ->
-            liftIO $ case (reportProject r, reportGithub r) of
-              (Nothing, Nothing) ->
-                pure ()
-              (Just p, Just _) ->
-                putStrLn $ "[OK] " <> (T.unpack . renderName . name) p
-              (Just p, Nothing) ->
-                putStrLn $ "[KO] no github repository found for " <> (T.unpack . renderName . name) p
-              (Nothing, Just g) ->
-                putStrLn $ "[KO] no anatomy metadata found for " <> repoName g)
---      DiagnoseCommand -> orDie renderReportError $ do -- FIX
---        r <- report github org projects
---        forM_ (syncables r) $
---          lift . T.putStrLn . renderProjectReport
+      -- done
+      ReportCommand -> orDie renderReportError $ do
+        github <- (GithubOAuth . T.unpack) <$> env "GITHUB_OAUTH"
+        rs <- report github org projects
+        liftIO $ forM_ rs $ \r ->
+          case (reportProject r, reportGithub r) of
+            (Nothing, Nothing) ->
+              pure ()
+            (Just p, Just _) ->
+              putStrLn $ "[OK] " <> (T.unpack . renderName . name) p
+            (Just p, Nothing) ->
+              putStrLn $ "[KO] no github repository found for " <> (T.unpack . renderName . name) p
+            (Nothing, Just g) ->
+              putStrLn $ "[KO] no anatomy metadata found for " <> repoName g
 
-      -- not done
-      SyncNewCommand -> orDie renderSyncError $ do -- FIX
-        -- FIX - add env var
-        -- basically the implementation for `--only-new`
-        r <- bimapEitherT SyncReportError id $ report github org projects
+      -- done
+      SyncNewCommand -> orDie renderSyncError $ do
+        github <- (GithubOAuth . T.unpack) <$> env "GITHUB_OAUTH"
+        hookz <- HooksUrl <$> env "JENKINS_HOOKS"
+        token <- HipchatToken <$> env "HIPCHAT_TOKEN"
+        room <- (HipchatRoom . T.pack) <$> (fromMaybe "eng" <$> liftIO (getEnv "HIPCHAT_ROOM_GITHUB"))
+        conf <- getJenkinsConfiguration
+
+        r <- bimapEitherT SyncReportError id $
+          report github org projects
         let n = newprojects r
-        bimapEitherT SyncGithubError id $ syncRepositories github templates org owners everyone n
-        bimapEitherT SyncCreateError id $ syncHooks github token room org hookz n
-        bimapEitherT SyncBuildError id $ syncBuilds jconf n
+        bimapEitherT SyncGithubError id $
+          syncRepositories github templates org owners everyone n
+        bimapEitherT SyncCreateError id $
+          syncHooks github token room org hookz n
+        bimapEitherT SyncBuildError id $
+          syncBuilds conf n
+        forM_ n $
+          liftIO . T.putStrLn . renderProjectReport
 
-      -- not done
-      -- FIX - add env var
+      -- done
       SyncCommand -> orDie renderSyncError $ do
-        r <- bimapEitherT SyncReportError id $ report github org projects
-        bimapEitherT SyncGithubError id . syncRepositories github templates org owners everyone $ newprojects r
-        -- Assume all projects exist now
-        bimapEitherT SyncCreateError id $ syncHooks github token room org hookz projects
-        bimapEitherT SyncBuildError id $ syncBuilds jconf projects
+        github <- (GithubOAuth . T.unpack) <$> env "GITHUB_OAUTH"
+        hookz <- HooksUrl <$> env "JENKINS_HOOKS"
+        token <- HipchatToken <$> env "HIPCHAT_TOKEN"
+        room <- (HipchatRoom . T.pack) <$> (fromMaybe "eng" <$> liftIO (getEnv "HIPCHAT_ROOM_GITHUB"))
+        conf <- getJenkinsConfiguration
 
-      -- partial done
+        r <- bimapEitherT SyncReportError id $
+          report github org projects
+        bimapEitherT SyncGithubError id .
+          syncRepositories github templates org owners everyone $ newprojects r
+        -- Assume all projects
+        bimapEitherT SyncCreateError id $
+          syncHooks github token room org hookz projects
+        bimapEitherT SyncBuildError id $
+          syncBuilds conf projects
+
+      -- done
       RefreshGithubCommand -> orDie renderSyncError $ do
         github <- (GithubOAuth . T.unpack) <$> env "GITHUB_OAUTH"
         hookz <- HooksUrl <$> env "JENKINS_HOOKS"
         token <- HipchatToken <$> env "HIPCHAT_TOKEN"
         room <- (HipchatRoom . T.pack) <$> (fromMaybe "eng" <$> liftIO (getEnv "HIPCHAT_ROOM_GITHUB"))
 
-        -- FIX investigate if this report is actually required
-        r <- bimapEitherT SyncReportError id $ report github org projects
-        bimapEitherT SyncCreateError id . syncHooks github token room org hookz $ hookable r
+        -- Creating report here is only safe way to ensure hooks does not fail.
+        r <- bimapEitherT SyncReportError id $
+          report github org projects
+        bimapEitherT SyncCreateError id .
+          syncHooks github token room org hookz $ hookable r
 
       -- done
       RefreshJenkinsCommand -> orDie renderBuildError $ do
@@ -124,7 +127,6 @@ getJenkinsConfiguration = liftIO $ do
   user <- JenkinsUser <$> env "JENKINS_USER"
   host <- JenkinsUrl <$> env "JENKINS_HOST"
   pure $ JenkinsConfiguration user jenkins host
-
 
 env :: MonadIO m => Text -> m Text
 env var =
@@ -139,21 +141,6 @@ env var =
 anatomy :: Parser Command
 anatomy =
   versionP <|> commandP
-
-{--
-
-
-  anatomy report
-
-  anatomy sync
-
-  anatomy sync --only-new
-
-  anatomy refresh-github-hooks
-
-  anatomy refresh-jenkins-jobs
-
---}
 
 commandP :: Parser Command
 commandP =  subparser $
